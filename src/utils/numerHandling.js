49a2b4806ffe6e3c2238b2b8878ff0486a94474c
arthurbritom <arthurmedeiros32@gmail.com>
2023-10-16 19:17:15 -0300

    fix: change number scale for avarage by blocks

--- src/utils/numerHandling.js
+++ src/utils/numerHandling.js
@@ -1,12 +1,12 @@
 export function approximateNumber(inputNumber) {
   const roundedNumber = Math.round(inputNumber);
-  if (roundedNumber >= 0 && roundedNumber <= 25) {
+  if (roundedNumber >= 0 && roundedNumber <= 10) {
       return 25;
-  } else if (roundedNumber >= 26 && roundedNumber <= 50) {
+  } else if (roundedNumber >= 11 && roundedNumber <= 30) {
       return 50;
-  } else if (roundedNumber >= 51 && roundedNumber <= 75) {
+  } else if (roundedNumber >= 31 && roundedNumber <= 50) {
       return 75;
-  } else if (roundedNumber > 75) {
+  } else if (roundedNumber > 51) {
       return 100;
   } else {
       console.error('Input number is out of valid range (below 0)');
